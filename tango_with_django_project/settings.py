5acf19d003a0375392059be635773735dbc3ebad
Danny Caulwell <caulwell@protonmail.com>
2021-07-26 20:30:59 +0100

    implement static files

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -69,6 +70,8 @@ TEMPLATES = [
     },
 ]
 
+
+
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 
@@ -118,5 +121,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+
+STATICFILES_DIRS = [STATIC_DIR,]
 
 STATIC_URL = '/static/'
