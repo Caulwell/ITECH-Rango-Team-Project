56f6552ef94f2cb08ee96f8564c7737c77175223
Danny Caulwell <caulwell@protonmail.com>
2021-07-28 16:36:04 +0100

    add page functionality

--- rango/forms.py
+++ rango/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from rango.models import Page, Category
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -15,8 +15,8 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
-    url=forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
+    url=forms.URLField(max_length=Page.URL_MAX_LENGTH, help_text="Please enter the URL of the page.")
     views=forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
@@ -30,3 +30,14 @@ class PageForm(forms.ModelForm):
         # We can either exclude the category field from the form
         exclude = ("category",)
         # or specify the fields to inlcude and don't include the category field - fields = ("title", "url", "views")
+
+        def clean(self):
+            cleaned_data = self.cleaned_data
+            url = cleaned_data.get("url")
+
+            #if url is not empty and doesn't start with "http://", prepend with it
+            if url and not url.startswith("http://"):
+                url = f"http://{url}"
+                cleaned_data["url"] = url
+            
+            return cleaned_data
