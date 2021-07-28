5a6d04c311c2b0f90c4e1f99bb6705528c3fddab
Danny Caulwell <caulwell@protonmail.com>
2021-07-28 17:50:39 +0100

    fix test failures

--- rango/views.py
+++ rango/views.py
@@ -16,7 +16,7 @@ def index(request):
     category_list = Category.objects.order_by("-likes")[:5]
     page_list = Page.objects.order_by("-views")[:5]
     context_dict = {}
-    context_dict["boldmessage"] = 'Crunchy, creamy, cookie, candie, cupcake!'
+    context_dict["boldmessage"] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict["categories"] = category_list
     context_dict["pages"] = page_list
 
