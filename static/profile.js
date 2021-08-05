da0ef742421ad281081cd0ed96a6f474745a0b9a
Iain <iainross24.7@gmail.com>
2021-08-05 14:22:19 +0100

    profile cosmetic changes

--- static/profile.js
+++ static/profile.js
@@ -1,16 +1,16 @@
 function show_update_url() {
     document.getElementById("update_url").style.display = "block";
-    document.getElementById("update_url_button").style.display = "none"
+
 }
 function hide_update_url() {
     document.getElementById("update_url").style.display = "none";
-    document.getElementById("update_url_button").style.display = "block"
+
 }
 function show_update_pic() {
     document.getElementById("update_pic").style.display = "block";
-    document.getElementById("update_pic_button").style.display = "none"
+
 }
 function hide_update_pic() {
     document.getElementById("update_pic").style.display = "none";
-    document.getElementById("update_pic_button").style.display = "block"
+
 }
